7eebbf8e1bf0a3e7ca1d821cd46629deccc96232
Evan BLANC <evanblanc.pro@icloud.com>
2021-10-24 12:46:46 -0400

    modif mod

--- INF1600/TP1/partie2.py
+++
@@ -1,8 +0,0 @@
-iteration = 8
-valeur = 10
-constante = 1
-
-for i in range(0, iteration, constante):
-    valeur -= constante
-    
-print(valeur)
--- INF1600/TP2/Partie_1/q1.1/lis_fibo.s
+++
@@ -1,9 +0,0 @@
-.text
-.globl lis_fibo
-
-lis_fibo:
-   movl 4(%esp),%eax 
-   leal adr_fibo, %ecx 
-   addl %ecx, %eax 
-   movl (%eax), %eax 
-   ret
