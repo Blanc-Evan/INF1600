a4952b4f767a2345723860c74c34588be2582d2d
Evan BLANC <evanblanc.pro@icloud.com>
2021-11-03 20:01:30 -0400

    hanoi

--- TP3/ex2_hanoi/hanoi_s_rec.s
+++ TP3/ex2_hanoi/hanoi_s_rec.s
@@ -11,7 +11,40 @@ hanoi_ASM_rec:
    pushl %ebx
    
 # DEBUT COMPLETION
+   movl 8(%ebp), %eax   //nb
+   movl 12(%ebp), %ebx  //from
+   movl 16(%ebp), %ecx // helper
+   movl 20(%ebp), %edx // target
+   cmpb $0, %eax
+   je Retour
+
+   subl $1, %eax  // nb - 1
    
+   pushl %ecx   //helper
+   pushl %edx   //target
+   pushl %ebx   //from
+   pushl %eax   //nb
+   call hanoi_ASM_rec
+
+   popl %eax   //nb
+   popl %ebx   //from
+   popl %edx   //target
+   popl %ecx   //helper
+
+   pushl %edx
+   pushl %ebx
+   call move
+
+   popl %ebx   //from
+   popl %edx   //target
+
+
+   pushl %edx   //target
+   pushl %ebx   //from
+   pushl %ecx   //helper
+   pushl %eax   //nb
+   call hanoi_ASM_rec
+
 # FIN COMPLETION   
    
 # NE RIEN MODIFIER APRES CETTE LIGNE
